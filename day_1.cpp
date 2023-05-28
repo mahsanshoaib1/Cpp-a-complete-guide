47a0f2d98c57b38a4589445da9ed9db7e3476183
mahsanshoaib1 <mahsanshoaib@protonmail.com>
2023-05-28 13:38:40 +0500

    this is the first day of cpp.

---
+++ day_1.cpp
@@ -0,0 +1,13 @@
+                                      // Writing my first code in cpp
+
+
+
+#include<iostream>      // this is called header file. iostream is a library which help us tu take input and display output
+using namespace std; 
+int main()              // main is a function and int is the return type of function. which is integer
+{
+    cout<<"Hello World";        // cout is used to display output, all the lines are called instructions
+    return 0;                   // return 0 means that the program is successfully executed
+}
+
+// the code or instructions in the curlibraces{} is called the block of code or the body of a function. 
