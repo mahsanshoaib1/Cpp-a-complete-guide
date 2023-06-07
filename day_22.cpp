a13283465170b3067876e3576d959dd7bfe611a6
mahsanshoaib1 <mahsanshoaib@protonmail.com>
2023-06-07 17:24:07 +0500

    arrays in class and static variable

---
+++ day_22.cpp
@@ -0,0 +1,42 @@
+// arrays in class and static variable
+
+#include <iostream>
+using namespace std;
+
+class shop
+{
+    int itemprice[100]={200};
+    int itemid[100] ={1001} ;
+    static int counter;        // static variable, by default 0. contains its previous value
+    public:
+    void price()
+    {
+        cout<<"The price of item is: "<<itemprice[0]<<endl;
+        
+    }
+    void id()
+    {
+        cout<<"The id of item is: "<<itemid[0]<<endl;
+        counter++;
+    }
+    void display()
+    {
+        cout<<"the counts of item is: "<<counter<<endl;
+    }
+
+
+};
+int shop :: counter;   // declare it in the class and outside the class as well.
+
+int main()
+{
+    shop obj1;
+    shop obj2;
+    obj1.id();
+    shop obj3;
+    obj1.id();
+    obj3.id();
+    obj1.price();
+    obj1.display();
+    return 0;
+}
